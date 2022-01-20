7df3b0366f29e5c08a0cb4c1b698930289b0c559
Gustavo Conde <guoconde@pop-os.localdomain>
2022-01-20 11:29:47 -0300

    Implementando extrutura inicial do projeto

---
+++ src/components/App/index.jsx
@@ -0,0 +1,13 @@
+import { ResetStyle } from "../../assets/css/Reset"
+import { GlobalStyle } from "../../assets/css"
+
+export default function App() {
+    return (
+        <>
+            <ResetStyle />
+            <GlobalStyle />
+
+            <h1>Estoy aqui</h1>
+        </>
+    )
+}
